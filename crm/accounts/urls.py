c3ea7fed25bb8da796c87035da66ce49103bc8f2
Moon4Light <chandrayadav0505@gmail.com>
2021-02-12 15:04:36 +0530

    added crud operations and dynamic routing

--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-
-#import views first to connect with urls
 from . import views
 
+
 urlpatterns = [
-    path('', views.home),
-    path('products/', views.products),
-    path('customer/', views.customer),
+    path('', views.home, name="home"),
+    path('products/', views.products, name='products'),
+    path('customer/<str:pk_test>/', views.customer, name="customer"),
+
+    path('create_order/', views.createOrder, name="create_order"),
+    path('update_order/<str:pk>/', views.updateOrder, name="update_order"),
+    path('delete_order/<str:pk>/', views.deleteOrder, name="delete_order"),
+
+
 ]
